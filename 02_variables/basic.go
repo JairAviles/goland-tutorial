32de94b6ed4e96c3bcd0921c3fbca36db8b97d66
Jair Aviles <jair.aviles@segment.com>
2026-01-30 12:50:34 -0800

    Add module 02

---
+++ 02_variables/basic.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var info = "Medium Havana Cappuccino is $3.75"
+
+	fmt.Println(info)
+
+	info = "Large Mocha Frappuccino is $4.00"
+
+	fmt.Println(info)
+}
