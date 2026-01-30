bc53e4efee1cbdcc982bdf246567bf7ad6c4a312
Jair Aviles <jair.aviles@segment.com>
2026-01-30 15:32:02 -0800

    zero values

---
+++ 02_variables/zero_values.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var totalOrders int
+	var customerName string
+	var isOrderReady bool
+
+	fmt.Println("Total orders quantity: ", totalOrders)
+	fmt.Println("Customer name: ", customerName)
+	fmt.Println("Is order ready?: ", isOrderReady)
+}
