91bfa49ec51be2c719dcdf49b8adf60d6c40ecb1
Jair Aviles <jair.aviles@segment.com>
2026-01-30 15:41:32 -0800

    multiple declarations

---
+++ 02_variables/multiple_declaration.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	//var coffee = 1
+	//var milk = 3
+	//var sugar = 1
+	var coffee, milk, sugar, isReady = 1, 3, 1, true
+
+	fmt.Println("Coffee:", coffee, "Milk:", milk, "Sugar:", sugar, "IsReady:", isReady)
+}
