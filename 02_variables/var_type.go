8e96b0d560914d5f91e6b9e160028fb8cdd40884
Jair Aviles <jair.aviles@segment.com>
2026-01-30 15:03:54 -0800

    implicit var definitions

---
+++ 02_variables/var_type.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	name := "Mocha"
+	price := 2.99
+	ready := true
+	count := 5
+
+	fmt.Printf("Type of name is: %T\n", name)
+	fmt.Printf("Type of price is: %T\n", price)
+	fmt.Printf("Type of ready is: %T\n", ready)
+	fmt.Printf("Type of count is: %T\n", count)
+}
