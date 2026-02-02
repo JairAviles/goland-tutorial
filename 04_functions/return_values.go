a9d158a47edc4ef6d797697bb0bba65cb13295f0
Jair Aviles <jair.aviles@segment.com>
2026-02-01 20:40:29 -0800

    return calculateLoyaltyPoints

--- 04_functions/return_values.go
+++ 04_functions/return_values.go
@@ -6,7 +6,14 @@ func calculateLoyaltyPoints(purchaseAmount float64) int {
 	return int(purchaseAmount * 2)
 }
 
+func updateTotalPoints(currentPoints int, pointsToAdd int) int {
+	return currentPoints + pointsToAdd
+}
+
 func main() {
-	earnedPoints := calculateLoyaltyPoints(9.50)
+	totalPoints := 120
+	earnedPoints := calculateLoyaltyPoints(9.30)
 	fmt.Println("Earned points today:", earnedPoints)
+
+	fmt.Println("Updated loyalty points:", updateTotalPoints(totalPoints, earnedPoints))
 }
